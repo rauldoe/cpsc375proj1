af7d2f71322fc2a21d82d616d1f6ae5cffbafcef
Khoa Do <kdo1@costar.com>
2019-12-02 12:21:50 -0800

    have to fix pass by ref issue

--- gencombo1.R
+++ gencombo1.R
@@ -12,13 +12,17 @@ buildIndex <- 0
 start <- 0
 end <- n
 comboList <- list()
+gComboList <- list()
 
 comboNChooseKRecursive <- function(cList, n, k, buildList, buildIndex, start, end, comboList) {
   
   if (buildIndex > k) {
 
   	cLength <- length(comboList) + 1
-  	comboList[[cLength]] <- rep(buildList)
+  	comboList[[cLength]] <- buildList
+  	cLength <- length(gComboList) + 1
+  	gComboList[[cLength]] <- buildList
+  	
 	  print("combo")
 	  print(buildList)
 	  
@@ -30,13 +34,14 @@ comboNChooseKRecursive <- function(cList, n, k, buildList, buildIndex, start, en
     	
     	buildList[buildIndex] <- cList[i]
     
-    	i <- i + 1
-    	buildIndex <- buildIndex + 1
+    	newI <- i + 1
+    	newBuildIndex <- buildIndex + 1
     
     	print(paste("buildIndex:", buildIndex, "i:", i, "end:", end, sep= " "))
       print(buildList)
-    	comboNChooseKRecursive(cList, n, k, buildList, buildIndex, i, end, comboList)
+    	comboNChooseKRecursive(cList, n, k, buildList, newBuildIndex, newI, end, comboList)
     	
+    	i <- i + 1
     }
     # while ((buildIndex <= k) && (i <= end) ) {
   }
@@ -47,4 +52,4 @@ comboNChooseKRecursive(cList, n, k, buildList, buildIndex, start, end, comboList
 
 
 print('combo')
-print(comboList)
+print(gComboList)
